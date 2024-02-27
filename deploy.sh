66ab8b752db48eb6b95f0bbc948caf4845080e0d
Isaac Sauri <isaacsauri@digitaldevops.com.mx>
2024-02-26 22:03:07 -0600

    ci update

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@
 
 
 
-echo "DROPLET_IP: $DROPLET_IP"
-echo "DROPLET_USERNAME: $DROPLET_USERNAME"
-echo "SSH_PRIVATE_KEY: $SSH_PRIVATE_KEY"
+echo "DROPLET_IP: ${secrets.DROPLET_IP}"
+echo "DROPLET_USERNAME: ${secrets.DROPLET_USERNAME}"
+echo "SSH_PRIVATE_KEY: ${secrets.SSH_PRIVATE_KEY}"
 
 
-echo -e "${SSH_PRIVATE_KEY}" > ssh_key.pem
+echo -e "${secrets.SSH_PRIVATE_KEY}" > ssh_key.pem
 chmod 600 ssh_key.pem
 
 # Copiar el archivo v1.js al Droplet
-scp -i ssh_key.pem v1.js $DROPLET_USERNAME@$DROPLET_IP:/library/v1
+scp -i ssh_key.pem v1.js ${secrets.DROPLET_USERNAME}@${secrets.DROPLET_IP}:/library/v1
 
 # Eliminar el archivo temporal de la clave privada
 rm -f ssh_key.pem
