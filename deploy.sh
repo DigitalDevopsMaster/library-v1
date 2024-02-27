ad2c8e6fe37c58669e9fa10c41631f44445919e9
Isaac Sauri <isaacsauri@digitaldevops.com.mx>
2024-02-26 21:57:09 -0600

    ci update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Crear un archivo temporal para la clave privada
+
+
+
+echo "DROPLET_IP: $DROPLET_IP"
+echo "DROPLET_USERNAME: $DROPLET_USERNAME"
+echo "SSH_PRIVATE_KEY: $SSH_PRIVATE_KEY"
+
+
 echo -e "${SSH_PRIVATE_KEY}" > ssh_key.pem
 chmod 600 ssh_key.pem
 
